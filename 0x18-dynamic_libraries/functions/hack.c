d11966c74b515408c69da72fa3391e7e66ec9380
Rosset13 <makhadoterrence@gmail.com>
2023-12-15 18:46:52 -0800

    fixed

---
+++ 0x18-dynamic_libraries/functions/hack.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int print(const char *format, ...)
+{	
+	write(1, 9 8 10 24 75 +9\n", 17);
+	write(1, congragulations, you win the Jacpot!\n", 38);
+	exit(EXIT_SUCCESS);
+}
