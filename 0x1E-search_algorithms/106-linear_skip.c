63cefc189a22045a4a2e8dd39027f37bffba264d
Rosset13 <makhadoterrence@gmail.com>
2024-05-22 12:20:24 -0700

    algo

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,45 +1,56 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - searches for a value in a skip list
+ * linear_skip - Searches for a value in a sorted singly linked list
+ *               of integers using linear skip.
+ * @list: A pointer to the head of the linked list to search.
+ * @value: The value to search for.
  *
- * @list: input list
- * @value: value to search in
- * Return: index of the number
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
  */
+
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *go;
+	skiplist_t *node, *jump;
 
+	/* Check if the list is NULL */
 	if (list == NULL)
 		return (NULL);
 
-	go = list;
-
-	do {
-		list = go;
-		go = go->express;
-		printf("Value checked at index ");
-		printf("[%d] = [%d]\n", (int)go->index, go->n);
-	} while (go->express && go->n < value);
-
-	if (go->express == NULL)
+	/* Perform linear skip */
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
 	{
-		list = go;
-		while (go->next)
-			go = go->next;
+		/* Store the previous jump node */
+		node = jump;
+
+		/* If the express lane is available */
+		if (jump->express != NULL)
+		{
+			/* Jump to the next express node */
+			jump = jump->express;
+
+			printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+		}
+		else
+		{
+			/* Traverse to the end of the list */
+			while (jump->next != NULL)
+				/* Move to the next node in the list */
+				jump = jump->next;
+		}
 	}
 
-	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-	while (list != go->next)
-	{
-		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
-		if (list->n == value)
-			return (list);
-		list = list->next;
-	}
+	/*  performs a linear search, checking each */
+	/*  node's value to find the target value */
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
-	return (NULL);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
 }
+
